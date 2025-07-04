0191dcf8b7a3699837714a98bd6ff2cf86f32122
ergomarketing <ergomarketing24@gmail.com>
2025-07-04 13:07:01 +0200

    Update client.ts

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Versión corregida y simplificada
-const supabase = createClient(
-  'https://qehractznaktxhyaqqen.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlaHJhY3R6bmFrdHhoeWFxcWVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NzgyNTgsImV4cCI6MjA2MjM1NDI1OH0.9CfIxbyJ3pLzWN2hDhCKGGAGAQ-JQoJUI8lQ9Kt1_kQ'
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
-
-export default supabase
