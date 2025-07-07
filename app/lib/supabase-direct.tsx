3656e2dec7a0025e6ef8fdee1be4eb90707d540d
ergomarketing <ergomarketing24@gmail.com>
2025-07-07 16:20:54 +0200

    Update supabase-direct.ts

--- app/lib/supabase-direct.tsx
+++ app/lib/supabase-direct.tsx
@@ -1,9 +1,8 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
-// Cliente exclusivo para backend con permisos elevados
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,       // URL pública del proyecto
-  process.env.SUPABASE_SERVICE_ROLE_KEY!       // Clave secreta (solo en backend)
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
 
 export { supabase }
