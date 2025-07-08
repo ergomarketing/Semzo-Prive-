225830716ab8f1e6b3b6d9c335f625412ecaf263
ergomarketing <ergomarketing24@gmail.com>
2025-07-08 21:57:30 +0200

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "../../lib/supabase-direct";
+import { supabase } from "../../lib/supabase-browser";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
