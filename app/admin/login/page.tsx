2879709f119b664762137089ee495ebd5db1fcfa
ergomarketing <ergomarketing24@gmail.com>
2025-08-18 13:08:55 +0200

    Update page.tsx

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from "react"
 import { useRouter } from "next/navigation"
-import { ADMIN_CONFIG } from "../../../config/email-config"
+import { ADMIN_CONFIG } from "../../config/email-config"
 
 
 export default function AdminLogin() {
